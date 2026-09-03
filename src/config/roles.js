43d931ac6290f1b5db567ad58b20695fc55b2c29
diabvass <vassiriki.diabate@epitech.eu>
2026-09-03 18:37:37 +0000

    Config : Definie les format pris en compte et les informations publiques de l'application

---
+++ src/config/roles.js
@@ -0,0 +1,12 @@
+export const Roles = {
+    Image: ['.jpeg', '.png', '.gif', '.webp', '.svg'],
+    Document: ['.txt', '.pdf', '.doc', '.docx', '.xls', '.ppt'],
+    Code: ['.html', '.css', '.js', '.json'],
+    Logiciel: ['.exe', '.msi']
+}
+
+export const INFOS_APP = {
+    NOM_APP: 'PhekiX',
+    DOSSIER_VEILLE: 'Downloads',
+    DOSSIER_DEPOT: 'PhekiX'
+}
--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import path from "path";
 import os from 'os'
 import env from 'dotenv'; env.config();
+import { INFOS_APP } from "./config/roles.js";
 import { Detecteur } from "./core/detecteur.js";
 
-const nom = process.env.NOM || 'PhekiX'
-const dirPath = path.join(os.homedir(), process.env.PATH_WATCH) || path.join(os.homedir(), 'Downloads') // doc surveillé
-console.log(`${nom} en écoute sur ${dirPath}`)
+const NOM_APP = INFOS_APP.NOM_APP
+const DOSSIER_VEILLE = INFOS_APP.DOSSIER_VEILLE
+const dirPath = path.join(os.homedir(), DOSSIER_VEILLE)
+
+console.log(`${NOM_APP} en écoute sur ${dirPath}`)
 
 Detecteur(dirPath); // top
