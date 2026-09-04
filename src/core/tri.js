414af73a293c163e611dbfc09f35d5d60e83453b
diabvass <vassiriki.diabate@epitech.eu>
2026-09-04 01:58:37 +0000

    trieur valide: cherche extension, fouille la liste Role et donne le type de du fichier

--- src/core/tri.js
+++ src/core/tri.js
@@ -1,3 +1,18 @@
-export const Trieur = async (filePath,depotPath) => {
+import path from 'path';
+import { Roles } from '../config/roles.js';
+import { Classeur } from './classeur.js'
+
+export const Trieur = async (filePath, depotPath) => {
+    const extension = path.extname(filePath);
+    let type = 'Autres';
+
+    for (const key in Roles) {
+        const extensions = Roles[key];
+        if (extensions.includes(extension)) {
+            type = key;
+            break;
+        }
+    }
+    await Classeur(type, filePath, depotPath)
 
 }
